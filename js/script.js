e870bbc7d501459b4b3daf5c713538100b0a6027
dev <166125538+Purohitdev@users.noreply.github.com>
2024-10-27 11:27:20 +0530

    events done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 // Put your js here or create a new file for it.
+
+
+
+// events start
+
+
+console.log("hello")
+
+const t1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".container ",
+        start: "top 50%",
+        end: "bottom 100%",
+        scrub: 1,
+    }
+})
+
+
+t1.from(".box",{
+    x:10,
+    stagger:0.2,
+    opacity:0,
+    
+
+})
+
+
+// events end
